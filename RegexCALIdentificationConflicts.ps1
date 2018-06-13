29a97398a1a0efaff76870c4e6aa9c27fd2809cb
arni ninja <arnif@lsretail.com>
2018-06-13 10:22:31 +0000

    Started writing regex matches for 4 cases

---
+++ RegexCALIdentificationConflicts.ps1
@@ -0,0 +1,30 @@
+function Match-Procedure ($Content, $Regex) {    
+#    PROCEDURE\ .*\@1200000001\(
+}
+
+function Match-Parameter($Content, $Regex) {
+
+}
+
+function Match-GlobalVariables ($Content, $Regex) {
+    
+}
+
+function Match-LocalVariables ($Content, $Regex) {
+    #PROCEDURE\ .*\n.*\n
+}
+
+function Fix-ProcedureIdConflict($Content, $Id){
+
+}
+function Match-ParameterIdConflict($Content, $Regex) {
+    
+}
+
+function Fix-GlobalVariableIdConflict($Content, $Id){
+    
+}
+
+function Fix-LocalVariableIdConflict($Content, $Id){
+    
+}
