6d937d826a1514bd6f32ee3d71b2aef2455a4a10
arni ninja <arnif@lsretail.com>
2018-06-13 19:40:32 +0000

    DocConflict fix prototype finished

--- RegexCALIdentificationConflicts.ps1
+++ RegexCALIdentificationConflicts.ps1
@@ -1,5 +1,5 @@
 function Match-Procedure ($Content, $Regex) {    
-#    PROCEDURE\ .*\@1200000001\(
+    # PROCEDURE\ .*\@1200000001\(
 }
 
 function Match-Parameter($Content, $Regex) {
@@ -7,16 +7,22 @@ function Match-Parameter($Content, $Regex) {
 }
 
 function Match-GlobalVariables ($Content, $Regex) {
-    
+    # CODE\n.*\{\n\s*VAR
+
+    # TO MATCH GLOBAL VAR
+    # Pattern above +
+    # First number after @ in any newline before a newline that is nothing but a linebreak
 }
 
 function Match-LocalVariables ($Content, $Regex) {
-    #PROCEDURE\ .*\n.*\n
+    # PROCEDURE\ .*\n.*\n
+
 }
 
 function Fix-ProcedureIdConflict($Content, $Id){
 
 }
+
 function Match-ParameterIdConflict($Content, $Regex) {
     
 }
